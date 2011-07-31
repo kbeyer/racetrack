e6bd52ec44c7d8413d96ea4602159661bf66eb05
kbeyer <kyle.beyer@gmail.com>
2011-07-30 20:42:51 -0500

    racetrack start

---
+++ grails-app/domain/racetrack/Race.groovy
@@ -0,0 +1,26 @@
+package racetrack
+
+class Race {
+	
+    static constraints = {
+		name(blank:false, maxSize:50)
+		startDate()
+		city()
+		state(inList:["GA", "NC", "SC", "VA"])
+		distance(min:0.0)
+		cost(min:0.0, max:100.0)
+		maxRunners(min:0, max:10000)
+	}
+	
+	String name
+	Date startDate
+	String city
+	String state
+	BigDecimal distance
+	BigDecimal cost
+	Integer maxRunners = 100000
+	
+	static mapping = {
+		sort "startDate"
+	}
+}
